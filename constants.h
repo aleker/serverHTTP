4a2e0612b5660976b458efae1f275e37e32653ae
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2017-02-15 15:59:09 +0100

    Trying to get answer from fcgi.

--- constants.h
+++ constants.h
@@ -7,34 +7,45 @@
 
 #include <stdio.h>
 
-ssize_t bufsize = 1000;
+size_t bufsize = 1000;
 
 #define FCGI_BEGIN_REQUEST       1
 #define FCGI_ABORT_REQUEST       2
-#define FCGI_END_REQUEST         3
+// #define FCGI_END_REQUEST         3
 #define FCGI_PARAMS              4
 #define FCGI_STDIN               5
-#define FCGI_STDOUT              6
-#define FCGI_STDERR              7
+//#define FCGI_STDOUT              6
+//#define FCGI_STDERR              7
 #define FCGI_DATA                8
 #define FCGI_GET_VALUES          9
-#define FCGI_GET_VALUES_RESULT  10
-#define FCGI_UNKNOWN_TYPE       11
-#define FCGI_MAXTYPE (FCGI_UNKNOWN_TYPE)
+//#define FCGI_GET_VALUES_RESULT  10
+//#define FCGI_UNKNOWN_TYPE       11
+//#define FCGI_MAXTYPE (FCGI_UNKNOWN_TYPE)
 
-#define FCGI_RESPONDER  1
-#define FCGI_AUTHORIZER 2
-#define FCGI_FILTER     3
+#define HEADER_SIZE             8
+// HEADER:
+#define VERSION                 0
+#define TYPE                    1
+#define REQUEST_ID_B1           2
+#define REQUEST_ID_B0           3
+#define CONTENT_LENGTH_B1       4
+#define CONTENT_LENGTH_B0       5
+#define PADDING_LENGTH          6
+#define RESERVED                7
 
-#define HEADER_SIZE         8
+#define BEGIN_REQUEST_BODY_SIZE 8
+// BEGIN REQUEST BODY:
+#define ROLE_B1                 0
+#define ROLE_B0                 1
+#define FLAGS                   2
+#define RESERVED_BEGIN          3
 
-#define VERSION             0
-#define TYPE                1
-#define REQUEST_ID_B1       2
-#define REQUEST_ID_B0       3
-#define CONTENT_LENGTH_B1   4
-#define CONTENT_LENGTH_B0   5
-#define PADDING_LENGTH      6
-#define RESERVED            7
+// ROLES:
+#define FCGI_RESPONDER          1
+#define FCGI_AUTHORIZER         2
+#define FCGI_FILTER             3
+
+// FLAGS:
+#define FCGI_KEEP_CONN          1
 
 #endif //SERWERHTTP_CONSTANTS_H
--- serwerTCP.cpp
+++ serwerTCP.cpp
@@ -22,23 +22,32 @@ void fillHeader(unsigned char* record, int type, int request_id, int contentLeng
     record[RESERVED] = 0;
 }
 
-int connectToFCGI3(int type, int requestId, int contentLength) {
-    cout << "---Wykonuję connectToFCGI3---\n";
-    int paddingLength = 8 - contentLength%8;
-    string message = "Marta jest super ekstra i Ola tez!11!";
-
-    // TODO upper boundary for message size
-    unsigned char record[HEADER_SIZE + contentLength + paddingLength];
-    fillHeader(record, type, requestId, contentLength, paddingLength);
-
+void fillContentData(unsigned char* record, unsigned char* content_data, int contentLength, int paddingLength) {
     for (int i = 0; i < contentLength; i++) {
-        record[i + HEADER_SIZE] = (unsigned char)message[i];
+        record[i + HEADER_SIZE] = content_data[i];
     }
-    for (int i = HEADER_SIZE+contentLength; i < sizeof(record); i++) {
+    for (int i = HEADER_SIZE+contentLength; i < (HEADER_SIZE + contentLength + paddingLength); i++) {
         record[i] = 0;
     }
+}
 
-    // TODO sending two messages
+void fillBeginRequestBody(unsigned char* record, int role, int flags) {
+    record[HEADER_SIZE + ROLE_B1] = (unsigned char) ((role >> 8) & 0xff);
+    record[HEADER_SIZE + ROLE_B0] = (unsigned char) ((role) & 0xff);
+    record[HEADER_SIZE + FLAGS] = (unsigned char) flags;
+    for (int i = HEADER_SIZE+RESERVED_BEGIN; i < HEADER_SIZE+BEGIN_REQUEST_BODY_SIZE; i++) {
+        record[i] = 0;
+    }
+}
+
+void createBeginMessage(unsigned char* record, int requestId, int role) {
+    fillHeader(record, FCGI_BEGIN_REQUEST, requestId, BEGIN_REQUEST_BODY_SIZE, 0);
+    fillBeginRequestBody(record, role, 0);
+}
+
+int connectToFCGI(int requestId, int contentLength, unsigned char* content_data) {
+    cout << "---Wykonuję connectToFCGI3---\n";
+
     int fd_fcgi = socket(PF_INET, SOCK_STREAM, 0);
     sockaddr_in fcgiSocket;
     fcgiSocket.sin_family = AF_INET;
@@ -49,11 +58,46 @@ int connectToFCGI3(int type, int requestId, int contentLength) {
         perror("Error connecting to socket");
         return -1;
     }
-    //cout << "ROZMIAR REKORDU: " << sizeof(record) << endl;
+    // --------------------------------------------------
+    // TODO upper boundary for message size
+    // TODO sending two messages
+    int paddingLength = (8 - contentLength%8)%8;
+    // SEND BEGIN MESSAGE
+    unsigned char record[HEADER_SIZE + BEGIN_REQUEST_BODY_SIZE];
+    createBeginMessage(record, requestId, FCGI_RESPONDER);
+    cout << "ROZMIAR REKORDU B: " << sizeof(record) << endl;
     sendto(fd_fcgi, &record, sizeof(record), 0, (sockaddr*)&fcgiSocket, sizeof(fcgiSocket));
+    free(record);
+
+    // SEND DATA
+    unsigned char record2[HEADER_SIZE + contentLength + paddingLength];
+    fillHeader(record2, FCGI_PARAMS, requestId, contentLength, paddingLength);
+    fillContentData(record2, content_data, contentLength, paddingLength);
+    cout << "ROZMIAR REKORDU P: " << sizeof(record2) << endl;
+    sendto(fd_fcgi, &record2, sizeof(record2), 0, (sockaddr*)&fcgiSocket, sizeof(fcgiSocket));
+
+//    // SEND DATA2
+//    unsigned char content_data2[] = {'a','b','c','d','e','f','g','h'};
+//    unsigned char record3[HEADER_SIZE + 8];
+//    fillHeader(record3, FCGI_PARAMS, requestId, 8, 0);
+//    fillContentData(record3, content_data2, 8, 0);
+//    cout << "ROZMIAR REKORDU P: " << sizeof(record3) << endl;
+//    sendto(fd_fcgi, &record3, sizeof(record3), 0, (sockaddr*)&fcgiSocket, sizeof(fcgiSocket));
+
+    // SEND DATA3
+    unsigned char record4[HEADER_SIZE];
+    fillHeader(record4, FCGI_PARAMS, requestId, 0, 0);
+    cout << "ROZMIAR REKORDU P: " << sizeof(record4) << endl;
+    sendto(fd_fcgi, &record4, sizeof(record4), 0, (sockaddr*)&fcgiSocket, sizeof(fcgiSocket));
+
+    // SEND DATA3
+    unsigned char record5[HEADER_SIZE];
+    fillHeader(record2, FCGI_STDIN, requestId, 0, 0);
+    cout << "ROZMIAR REKORDU S: " << sizeof(record5) << endl;
+    sendto(fd_fcgi, &record5, sizeof(record5), 0, (sockaddr*)&fcgiSocket, sizeof(fcgiSocket));
+
     cout << "---Po sendto. Wychodzę z connectToFCGI---\n";
 }
-
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -79,9 +123,8 @@ int main(int argc, char** argv) {
     socklen_t sizeOfAcceptedSockaddr = 0;
     int enable = 1;
 
-    int RecvMsgSize = 0;
-    int Len;
-    char buffer[bufsize];
+    unsigned char content_data[bufsize];
+    int id = 300;
 
     if (listen(fd, 10) == 0) {
         while (true) {
@@ -90,13 +133,13 @@ int main(int argc, char** argv) {
             setsockopt(acceptedSocketFd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)); // There will be no TIME_WAIT
 
             // READING MESSAGE FROM CLIENT
-            Len = read(acceptedSocketFd, buffer, bufsize);
-            buffer[Len] = 0;// make sure it's a proper string
-            cout<< "\nWIADOMOŚĆ OD PRZEGLĄDARKI\n" << buffer << "KONIEC WIADOMOŚCI\n\n" << endl;
+            ssize_t Len = read(acceptedSocketFd, content_data, bufsize);
+            content_data[Len] = 0;// make sure it's a proper string
+            cout<< "\nWIADOMOŚĆ OD PRZEGLĄDARKI: " << sizeof(content_data) << endl << content_data << "KONIEC WIADOMOŚCI\n\n" << endl;
 
             // RUNNING FCGI:
             //--- http://stackoverflow.com/questions/26695738/nginx-fastcgi-without-using-spawn-fcgi
-            connectToFCGI3(FCGI_BEGIN_REQUEST, 300, 37);
+            connectToFCGI(id, sizeof(content_data), content_data);
             // SENDING ANSWER TO CLIENT
             // ssize_t sentBytes = write(acceptedSocketFd, &buf, sizeof(buf));
             // cout << "Sent bytes: " << sentBytes << endl;
