05a096d3dbabe5994b3446ab7392a401a7e1c7ca
mpralat <pralatmarta@gmail.com>
2017-02-23 19:32:21 +0100

    Cleaned a byte (hehe)

--- FCGIManager.cpp
+++ FCGIManager.cpp
@@ -35,10 +35,10 @@ void FCGIManager::sendMessage(int clientSocketFd) {
     unsigned char end_header[8];
     end_header[0] = FCGI_VERSION;
     end_header[1] = FCGI_END_REQUEST;
-    end_header[4] = ZERO;      // content length1
-    end_header[5] = 8;      // content length0
-    end_header[6] = ZERO;      // padding length
-    end_header[7] = ZERO;      // reserved
+    end_header[4] = ZERO;               // content length - always eight
+    end_header[5] = 8;                  // content length - always eight
+    end_header[6] = ZERO;               // padding length - always zero
+    end_header[7] = ZERO;               // reserved - always zero
     // Copying the two bytes of requestId
     recv(descriptor, &message_buf, 8, 0);
     end_header[2] = message_buf[2];
@@ -47,17 +47,18 @@ void FCGIManager::sendMessage(int clientSocketFd) {
     bool stop_reading = false;
     while (1) {
         // Reading 8 bytes at a time
+        // TODO do sth with readBytes == 0
         readBytes = recv(descriptor, &message_buf, sizeof(message_buf), 0);
         if (strcmp((const char *) message_buf, (const char *) end_header) == 0) {
             stop_reading = true;
         } else {
-            if(stop_reading) {
+            if (stop_reading) {
                 // get the appStatus here
                 int appStatus = int(message_buf[0] << 24 |
-                                  message_buf[1] << 16 |
-                                  message_buf[2] << 8  |
-                                  message_buf[3]);
-                std::cout << "I HAVE RECEIVED THE APP STATUS (for some reason, duh)! " << appStatus << std::endl;
+                                    message_buf[1] << 16 |
+                                    message_buf[2] << 8 |
+                                    message_buf[3]);
+                std::cout << "appStatus " << appStatus << std::endl;
                 break;
             }
             try {
@@ -69,21 +70,9 @@ void FCGIManager::sendMessage(int clientSocketFd) {
                 return;
             }
         }
-
     }
     std::cout << "***END OF MESSAGE FROM FCGI TO CLIENT\n";
 
-//    // TODO sparsować wiadomość od FCGI
-//    // TODO czytać do FCGI_END_REQUEST
-//    std::cout << "\n***MESSAGE FROM FCGI TO CLIENT\n";
-//    unsigned char message_buf[8];
-//    ssize_t readBytes = 0;
-//    std::string message_from_fcgi;
-//    while ((readBytes = recv(descriptor, &message_buf, sizeof(message_buf), 0)) != 0) {
-//        std::string received(message_buf, message_buf + readBytes);
-//        message_from_fcgi.append(received);
-//    }
-//    std::cout << "***END OF MESSAGE FROM FCGI TO CLIENT\n";
 }
 
 FCGIManager::~FCGIManager() {}
